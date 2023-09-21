70a560e1d16c51948cd0a3131a42f30ffcf102a5
Kalpit Sahu <kalpitsahu22.com>
2023-09-21 14:18:57 +0530

    uselogout route

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,20 +1,16 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
-import toast from "react-hot-toast";
+import { toast } from "react-hot-toast";
 
 export function useLogin() {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
+
   const { mutate: login, isLoading } = useMutation({
-    mutationFn: ({ email, password }) =>
-      loginApi({
-        email,
-        password,
-      }),
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       queryClient.setQueryData(["user"], user.user);
-      console.log(user);
       navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
@@ -22,5 +18,6 @@ export function useLogin() {
       toast.error("Provided email or password are incorrect");
     },
   });
+
   return { login, isLoading };
 }
